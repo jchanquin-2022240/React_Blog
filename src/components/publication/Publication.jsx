03af49e1826c38849d227a1aebe69733f5a63fe6
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-05-11 21:55:45 -0600

    html de Publication.jsx y el DashboardPage la llamada

---
+++ src/components/publication/Publication.jsx
@@ -0,0 +1,44 @@
+import './publication.css'
+
+export const Publication = ({ publication }) => {
+    return (
+        <div>
+            {publication.map((publication, index) => (
+                <div className='post-card' key={index}>
+                    <div className='container-card'>
+                        <label>Título</label>
+                        <div>{publication.title}</div>
+                    </div>
+                    <div className='container-card'>
+                        <label>author</label>
+                        <div>{publication.author}</div>
+                    </div>
+                    <div className='container-card'>
+                        <label>description</label>
+                        <div>{publication.description}</div>
+                    </div>
+                    <div className='container-card'>
+                        <label>Languaje and tools</label>
+                        <div>{publication.tools}</div>
+                    </div>
+                    <div className='container-card'>
+                        <label>Function</label>
+                        <div>{publication.descriptionFuntion}</div>
+                    </div>
+                    <div className='container-card'>
+                        <label>Image</label>
+                        <img src={publication.image} alt="Image" />
+                    </div>
+                    <div className='container-card'>
+                        <label>link</label>
+                        <a href={publication.link}></a>
+                    </div>
+                    <div className='container-card'>
+                        <label>Date</label>
+                        <div>{publication.date}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useSate, useEffect } from 'react';
 import { Navbar} from '../../components/navbar/Navbar';
 import { Footer } from '../../components/footer/Footer';
+import { getPost } from '../../services';
+import { Publication } from '../../components/publication/Publication';
 
 export const DashboardPage = () => {
+    const [publication, setPublication] = useState([]);
+
+    useEffect(() => {
+        const fetchPublications = async () => {
+            try {
+                const response = await getPost();
+                console.log(response)
+                if (!response.error) {
+                    setPublication(response.data || []);
+                } else {
+                    console.log('Error:', response.data);
+                }
+            } catch (error) {
+                console.log('Error fetching publications:', error);
+            }
+        };
+        fetchPublications();
+    }, []);
+
     return (
-        <Navbar/>
+        <Publication publication={publication}/>
     )
 }
