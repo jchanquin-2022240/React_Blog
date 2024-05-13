21394731c3d312d6c6e3fdd098d70f8d362faa01
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-05-12 20:19:50 -0600

    footer con estilo terminado

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import './footer.css'
 
 export const Footer = () => {
     return (
-        <footer className="footer">
+        <footer className="footer-container">
             <div className="copyright">
                 © 2024 Copyright:
                 <a>Rodrigo Chanquín</a>
